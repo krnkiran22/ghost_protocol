e2819bafe18d36c0dbe22b516226220f04d5ae1d
krnkiran22 <kirandev2210@gmail.com>
2025-10-12 23:20:51 +0530

    need to integrate the contracts

--- src/store/useRegistrationStore.ts
+++ src/store/useRegistrationStore.ts
@@ -201,6 +201,7 @@ export const useRegistrationStore = create<RegistrationState>((set, get) => ({
     }));
   },
 
+  
   // File upload completion handler
   handleUploadComplete: (data: { 
     title: string;
